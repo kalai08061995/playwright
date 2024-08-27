d849588959a7099346e5cb84d3231b1d4d9fd9e8
kalai08061995 <kalaiselvi.senthilvel@expleogroup.com>
2024-08-27 14:17:55 +0530

    commited

---
+++ tests/login.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+
+//launch the login URL
+test('Login Url', async ({ page }) => {
+  await page.goto('https://demo.cyclos.org/ui/login');
+  
+
+  // Fill the username and password 
+  await page.fill('//input[@autocomplete="username"]',"demo");
+  await page.fill('//input[@autocomplete="password"]',"1234");
+
+
+  //Click the submit button
+  await page.click('//*[contains(text(),"Submit")]');
+});
+
+
