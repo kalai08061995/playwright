4a3fbd5b37a7f6f21e2406ab31db4535d34710e3
kalai08061995 <kalaiselvi.senthilvel@expleogroup.com>
2024-08-28 11:18:34 +0530

    commets line updated

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 
+
 //launch the login URL
 test('Login Url', async ({ page }) => {
   await page.goto('https://demo.cyclos.org/ui/login');
   
 
-  // Fill the username and password 
+  // Fill the username and password field 
   await page.fill('//input[@autocomplete="username"]',"demo");
   await page.fill('//input[@autocomplete="password"]',"1234");
 
